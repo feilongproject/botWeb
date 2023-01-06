e44f2d79dd52ad4971e49fe6f87100483b55dadb
feilongproject <flzs6137@gmail.com>
2023-01-06 13:52:28 +0800

    feat: 添加关键词相关功能, 优化代码逻辑与样式

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,8 +4,8 @@
             <tr>
                 <th>功能</th>
                 <th>数据输入</th>
-                <th>选择发送对象</th>
-                <th>开始发送!</th>
+                <th>选择操作对象</th>
+                <th>开始操作</th>
             </tr>
             <tr>
                 <th><label for="sendMsgContent">向子频道发送消息</label></th>
@@ -13,17 +13,46 @@
                     <textarea id="sendMsgContent" name="sendMsgContent" class="sendMsgContent" v-model="sendMsgContent"></textarea>
                 </th>
                 <th style="width: 30%">
-                    <label for="channelSelect">选择子频道</label>
-                    <select id="channelSelect" style="width: 50%" v-model="channelInfo.selectd" @change="selectChannel" required>
-                        <option v-if="channelInfo.all.length != 0 && channelInfo.selectd == 'NONE'" value="NONE">未选择</option>
-                        <option v-for="(options, id) in channelInfo.all" :key="id" :value="options.id">{{ options.name }}</option>
-                    </select>
+                    <label for="channelSelect">
+                        <select id="channelSelect" style="width: 50%" v-model="channelInfo.selectd" @change="selectChannel" required>
+                            <option key="NONE" value="NONE">未选择</option>
+                            <option v-for="(options, id) in channelInfo.all" :key="id" :value="options.id">{{ options.name }}</option>
+                        </select>
+                    </label>
                 </th>
                 <th>
                     <button @click="sendMsg">点我发送</button>
                     <hr />
                     <div>发送状态</div>
-                    <div :style="sendedClass">{{ sendedInfo }}</div>
+                    <div :style="sended.class">{{ sended.info }}</div>
+                </th>
+            </tr>
+            <tr>
+                <th><label for="keywordContent">关键词查询</label></th>
+                <th>
+                    <textarea id="keywordContent" name="keywordContent" class="sendMsgContent" v-model="keywords.selectd.content" v-bind:readonly="keywords.readonly"></textarea>
+                </th>
+                <th>
+                    <label for="keywordSelect">
+                        <select id="keywordSelect" style="width: 50%" v-model="keywords.selectd.index" @change="selectKeyword" required>
+                            <option key="NONE" value="0" v-if="keywords.selectd.keyword == 'NONE'">未选择</option>
+                            <option v-for="(options, id) in keywords.all" :key="id" :value="id">{{ options.keyword }}</option>
+                        </select>
+                        <div v-if="keywords.selectd.keyword != 'NONE'">
+                            <!-- <span>{{ keywords.selectd }}</span> -->
+                            <div>类型: {{ keywords.selectd.type == "accurate" ? "精确" : "模糊" }}</div>
+                            <div>审核状态: {{ keywords.selectd.status == "checked" ? "通过" : "正在审核" }}</div>
+                            <div>创建者: {{ keywords.selectd.ownerName }}</div>
+                        </div>
+                    </label>
+                </th>
+                <th>
+                    <button @click="keywordExec.passCheck">点我通过审核</button>
+                    <button @click="keywordExec.saveContent">点我保存回复内容</button>
+                    <button @click="keywordExec.delete">点我删除</button>
+                    <hr />
+                    <div>发送状态</div>
+                    <div :style="keywordExec.status.value.class">{{ keywordExec.status.value.info }}</div>
                 </th>
             </tr>
         </table>
@@ -36,13 +65,18 @@
         </div>
         <div class="botServer">
             <span>bot服务器连接信息</span>
-            <div><label for="botServerInputIp">地址</label> <input id="botServerInputIp" type="text" v-model="botServerIp" /></div>
-            <div><label for="botServerInputPort">端口</label> <input id="botServerInputPort" type="text" v-model="botServerPort" /></div>
-            <div><label for="botServerInputToken">Token</label> <input id="botServerInputToken" type="text" v-model="botServerToken" /></div>
-            <div>
+            <label class="botServerWsType">
+                连接类型
+                <span class="input"> <input id="botServerWsTypeWs" v-model="botServer.wsType" type="radio" value="ws" /> ws <input id="botServerWsTypeWss" v-model="botServer.wsType" type="radio" value="wss" />wss </span>
+            </label>
+            <label for="botServerInputIp">地址<input id="botServerInputIp" type="text" v-model="botServer.ip" /></label>
+            <label for="botServerInputPort">端口<input id="botServerInputPort" type="text" v-model="botServer.port" /></label>
+            <label for="botServerInputToken">Token<input id="botServerInputToken" type="text" v-model="botServer.token" /></label>
+            <div class="buttons">
                 <button @click="saveServer">保存服务器设置</button>
                 <button @click="closeServer">关闭连接</button>
-                <button @click="changeReconnent">{{ botServerReconnent ? "关闭" : "开启" }}自动重连</button>
+                <button @click="refreshData">刷新数据</button>
+                <button @click="changeReconnent">{{ botServer.reConnent ? "关闭" : "开启" }}自动重连</button>
             </div>
         </div>
     </div>
@@ -57,20 +91,59 @@ import Notice from "./Notice.vue";
 
 const noticeShow = ref();
 const botStatus = ref([{ msg: "正在建立连接", classType: "0af" }]);
-const botServerIp = ref(localStorage.getItem("botServerIp") || "127.0.0.1");
-const botServerPort = ref(localStorage.getItem("botServerPort") || "8848");
-const botServerToken = ref(localStorage.getItem("botServerToken") || "");
-const botServerReconnent = ref(true);
-const sendMsgContent = ref("");
-const sendedInfo = ref("未发送");
-const sendedClass = ref("border: solid 3px #0af");
+const botServer = ref({
+    token: localStorage.getItem("botServerToken") || "",
+    port: localStorage.getItem("botServerPort") || "8848",
+    ip: localStorage.getItem("botServerIp") || "127.0.0.1",
+    wsType: localStorage.getItem("botServerWsType") || "ws",
+    reConnent: localStorage.getItem("reConnent") == "false" ? false : true,
+});
 const channelInfo = ref({
     selectd: "NONE",
     all: [{ id: "NONE", name: "未选择", selected: true, type: 0 }],
 });
-var ws = new WebSocket(`ws://${botServerIp.value}:${botServerPort.value}?token=${botServerToken.value}`, botServerToken.value ? [botServerToken.value] : undefined);
-
-const wsIntentMessage: { [key: string]: (data: any) => void } = {
+channelInfo.value.all = [];
+const sendMsgContent = ref("");
+const sended = ref({
+    info: "未发送",
+    class: "border: solid 3px #0af",
+});
+const keywords = ref({
+    readonly: true,
+    selectd: { index: 0, keyword: "NONE", content: "", ownerId: "", ownerName: "", refOwnerId: "", refOwnerName: "", status: "", type: "" },
+    all: [{ keyword: "未选择", content: "", ownerId: "", ownerName: "", refOwnerId: "", refOwnerName: "", status: "", type: "" }],
+});
+keywords.value.all = [];
+const keywordExec = {
+    passCheck: () => {
+        if (keywords.value.selectd.keyword == "NONE") return (keywordExec.status.value = { info: "未选择数据, 无法通过", class: "border: solid 3px #f00" });
+        wsSend({ key: "keyword.passCheck", data: keywords.value.selectd }, "keyword.passCheck");
+    },
+    saveContent: () => {
+        if (keywords.value.selectd.keyword == "NONE") return (keywordExec.status.value = { info: "未选择数据, 无法保存", class: "border: solid 3px #f00" });
+        wsSend({ key: "keyword.saveContent", data: keywords.value.selectd }, "keyword.saveContent");
+    },
+    delete: () => {
+        if (keywords.value.selectd.keyword == "NONE") return (keywordExec.status.value = { info: "未选择数据, 无法删除", class: "border: solid 3px #f00" });
+        wsSend({ key: "keyword.delete", data: keywords.value.selectd }, "keyword.delete");
+    },
+    status: ref({
+        info: "未操作",
+        class: "border: solid 3px #0af",
+    }),
+};
+//var intervalId: NodeJS.Timer | number | null = null;
+var ws: WebSocket | null = null;
+const wsIntentMessage: { [key: string]: (data?: any) => void } = {
+    ok: () => {
+        intervalHandler();
+        // if (intervalId) clearInterval(intervalId);
+        // intervalHandler();
+        // intervalId = setInterval(intervalHandler, 10 * 1000);
+    },
+    error: (err) => {
+        botStatus.value.push({ msg: `Error: ${err}`, classType: "f00" });
+    },
     "channel.getList": (data: SaveGuild[]) => {
         //console.log(`接收到信息:`, data);
         channelInfo.value.all = [];
@@ -87,50 +160,139 @@ const wsIntentMessage: { [key: string]: (data: any) => void } = {
     },
     "channel.recMsg": (data) => {
         //console.log("channel.recMsg", data);
-        sendedInfo.value = `发送成功`;
-        sendedClass.value = "border: solid 3px #0af";
+        sended.value = { info: `发送成功`, class: "border: solid 3px #0af" };
+    },
+    "keyword.get": (_keywords) => {
+        keywords.value.all = _keywords;
+    },
+    "keyword.passCheck": (_keyword) => {
+        keywords.value.all[_keyword.index] = _keyword;
+        keywords.value.selectd = _keyword;
+        if (_keyword.status == "checked") keywordExec.status.value = { info: `已通过审核`, class: "border: solid 3px #0af" };
+    },
+    "keyword.saveContent": (_keyword) => {
+        keywordExec.status.value = { info: `已保存${_keyword.type == "accurate" ? "精确" : "模糊"}关键词"${_keyword.keyword}"的内容`, class: "border: solid 3px #0af" };
+    },
+    "keyword.delete": async (_keyword) => {
+        if (!_keyword.isDel) return;
+        keywords.value.readonly = true;
+        keywordExec.status.value = { info: `已删除${_keyword.type == "accurate" ? "精确" : "模糊"}关键词"${_keyword.keyword}"`, class: "border: solid 3px #0af" };
+        intervalHandler();
+        keywords.value.selectd = { index: 0, keyword: "NONE", content: "", ownerId: "", ownerName: "", refOwnerId: "", refOwnerName: "", status: "", type: "" };
     },
 };
-
 watch(botStatus.value, () => {
-    while (botStatus.value.length > 5) botStatus.value.shift();
+    while (botStatus.value.length > 10) botStatus.value.shift();
 });
 
+function intervalHandler() {
+    //if (!botServer.value.autoFetch) return;
+    wsSend({ key: "channel.getList" }, "channel.getList");
+    wsSend({ key: "keyword.get" }, "keyword.get");
+}
+function wsSend(content: { key: string; retKey?: string; data?: {} }, desc: string) {
+    botStatus.value.push({
+        msg: `于${new Date().toLocaleString()}发送数据: ${desc}`,
+        classType: "0af",
+    });
+    ws!.send(JSON.stringify(content));
+}
+
+function saveServer(e: MouseEvent) {
+    localStorage.setItem("botServerIp", botServer.value.ip);
+    localStorage.setItem("botServerPort", botServer.value.port);
+    localStorage.setItem("botServerToken", botServer.value.token);
+    localStorage.setItem("botServerWsType", botServer.value.wsType);
+    //localStorage.setItem("autoFetch", `${botServer.value.autoFetch}`);
+    localStorage.setItem("reConnent", `${botServer.value.reConnent}`);
+}
+function closeServer(e: MouseEvent) {
+    ws!.close(1000, "客户端主动关闭");
+}
+function refreshData(e: MouseEvent) {
+    intervalHandler();
+}
+function changeReconnent() {
+    botServer.value.reConnent = !botServer.value.reConnent;
+    botStatus.value.push({ msg: `已${botServer.value.reConnent ? "开启" : "关闭"}自动重连`, classType: "00f" });
+    if (botServer.value.reConnent) init();
+}
+
+function selectChannel(e: Event) {
+    //console.log(e, channelInfo.value);
+}
+function selectKeyword(e: Event) {
+    //console.log((e.target as any).options);
+    //console.log(keywords.value.selectd);
+    keywords.value.readonly = false;
+    const _index = (e.target as any).selectedIndex;
+    const index = keywords.value.selectd.content == "" ? _index - 1 : _index;
+    keywords.value.selectd = Object.assign({ index }, keywords.value.all[index]);
+}
+
+function sendMsg() {
+    // console.log(noticeShow.value);
+    console.log(`发送至: ${channelInfo.value.selectd}, 发送内容: ${sendMsgContent.value}`);
+    if (channelInfo.value.selectd == "NONE") {
+        sended.value = { info: "错误: 未指定发送频道", class: "border: solid 3px #f00" };
+    } else if (!sendMsgContent.value) {
+        sended.value = { info: "错误: 发送内容为空", class: "border: solid 3px #f00" };
+    } else {
+        sended.value = { info: "发送中", class: "border: solid 3px #00f" };
+        wsSend(
+            {
+                key: "channel.postMsg",
+                retKey: "channel.recMsg",
+                data: {
+                    channelId: channelInfo.value.selectd,
+                    content: sendMsgContent.value,
+                },
+            },
+            "channel.postMsg"
+        );
+    }
+
+    // noticeShow.value.noticeShow = !noticeShow.value.noticeShow;
+}
+
 init();
-function init(isReconnent = false) {
-    if (isReconnent) ws = new WebSocket(`ws://${botServerIp.value}:${botServerPort.value}?token=${botServerToken.value}`, botServerToken.value ? [botServerToken.value] : undefined);
+function init() {
+    ws = new WebSocket(`${botServer.value.wsType}://${botServer.value.ip}:${botServer.value.port}`, botServer.value.token ? [botServer.value.token] : undefined);
 
     ws.onopen = () => {
-        console.log("连接开启，状态：", ws.readyState);
+        console.log("连接开启，状态：", ws!.readyState);
         botStatus.value.push({
             msg: `连接开启`,
             classType: "0af",
         });
-        ws.send(JSON.stringify({ key: "channel.getList" }));
     };
     ws.onmessage = (e) => {
-        botStatus.value.push({
-            msg: `于${new Date().toLocaleString()}接收数据`,
-            classType: "0af",
-        });
         const data = JSON.parse(e.data);
+        botStatus.value.push({
+            msg: `于${new Date().toLocaleString()}接收数据: ${data.key}`,
+            classType: "0f0",
+        });
         console.log(`接收数据:`, data);
         wsIntentMessage[data.key](data.data);
     };
     ws.onclose = (e) => {
+        // if (intervalId) {
+        //     clearInterval(intervalId);
+        //     intervalId = null;
+        // }
         const closeInfo = `${e.reason ? e.reason : "未知原因"} | 连接被关闭, 正在重新连接`;
         console.log(closeInfo);
         botStatus.value.push({ msg: closeInfo, classType: "f00" });
         (function aaa(times: number) {
             setTimeout(() => {
-                if (!botServerReconnent.value) return;
-                console.log(`重新连接中, 等待${times}s, 服务器状态: ${ws.readyState}`);
+                if (!botServer.value.reConnent) return;
+                console.log(`重新连接中, 等待${times}s, 服务器状态: ${ws!.readyState}`);
                 botStatus.value.push({
                     msg: closeInfo + `\n重新连接中, 等待${times}s`,
                     classType: "f00",
                 });
                 if (times) aaa(times - 1);
-                else init(true);
+                else init();
             }, 1000);
         })(5);
     };
@@ -139,56 +301,6 @@ function init(isReconnent = false) {
     botStatus.value = "连接发生错误";
     botError.value = "连接发生错误";
 }; */
-}
-
-function saveServer(e: MouseEvent) {
-    localStorage.setItem("botServerIp", botServerIp.value);
-    localStorage.setItem("botServerPort", botServerPort.value);
-    localStorage.setItem("botServerToken", botServerToken.value);
-}
-function closeServer(e: MouseEvent) {
-    ws.close(1000, "客户端主动关闭");
-}
-
-function selectChannel(e: Event) {
-    //console.log(e, channelInfo.value);
-}
-
-function changeReconnent() {
-    botServerReconnent.value = !botServerReconnent.value;
-    botStatus.value.push({ msg: `已${botServerReconnent.value ? "开启" : "关闭"}自动重连`, classType: "00f" });
-    if (botServerReconnent.value) init(true);
-}
-
-function sendMsg() {
-    // console.log(noticeShow.value);
-    console.log(`发送至: ${channelInfo.value.selectd}, 发送内容: ${sendMsgContent.value}`);
-    if (channelInfo.value.selectd == "NONE") {
-        sendedInfo.value = "错误: 未指定发送频道";
-        sendedClass.value = "border: solid 3px #f00";
-    } else if (!sendMsgContent.value) {
-        sendedInfo.value = "错误: 发送内容为空";
-        sendedClass.value = "border: solid 3px #f00";
-    } else {
-        botStatus.value.push({
-            msg: `于${new Date().toLocaleString()}发送数据`,
-            classType: "0af",
-        });
-        sendedInfo.value = "发送中";
-        sendedClass.value = "border: solid 3px #00f";
-        ws.send(
-            JSON.stringify({
-                key: "channel.postMsg",
-                retKey: "channel.recMsg",
-                data: {
-                    channelId: channelInfo.value.selectd,
-                    content: sendMsgContent.value,
-                },
-            })
-        );
-    }
-
-    // noticeShow.value.noticeShow = !noticeShow.value.noticeShow;
 }
 
 interface SaveGuild {
@@ -212,7 +324,6 @@ interface SaveChannel {
 
 .botStatusInfo {
     display: flex;
-    height: 300px;
     width: 70%;
     flex-direction: column;
     justify-content: space-around;
@@ -223,13 +334,19 @@ interface SaveChannel {
 .sendMsg {
     display: flex;
     justify-content: space-evenly;
+    textarea:read-only {
+        background-color: #000;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: url("/noData.svg");
+    }
 }
 
 .sendMsgContent {
     width: 100%;
     color: #fff;
     background-color: #333;
-    height: 200px;
+    height: 150px;
 }
 
 .botServer {
@@ -238,12 +355,21 @@ interface SaveChannel {
     align-items: flex-end;
     border: solid 3px #0af;
 
+    .botServerWsType {
+        .input {
+            padding: 0px;
+        }
+        input {
+            margin: 0px;
+        }
+    }
+
     span,
-    div:last-child {
+    .buttons {
         align-self: center;
     }
 
-    div:last-child {
+    .buttons {
         display: flex;
         flex-direction: column;
     }
